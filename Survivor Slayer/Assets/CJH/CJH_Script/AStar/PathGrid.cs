015427ce3f18e90e1535f4861e7ed28d3a481afb
Choi Jae Hyuck <cgear@naver.com>
2022-03-07 00:51:12 +0900

    A* 맵 표기 및 루트 탐색
    A* Grid를 표시하고 start-target을 설정해서 루트를 그리도록 만들어놈
    다음목표 : 루트를 바탕으로 enemy가 이동하도록 설정

---
+++ Survivor Slayer/Assets/CJH/CJH_Script/AStar/PathGrid.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathGrid : MonoBehaviour
+{
+   public Transform startPositon;
+   public LayerMask WallMask;
+   public Vector2 gridWorldSize;
+   public float nodeRadius;
+   public float Distance;
+
+   Node[,] grid;
+   public List<Node> FinalPath;
+
+   private float nodeDiameter;
+   private int gridSizeX, gridSizeY;
+
+   private void Start()
+   {
+      nodeDiameter = nodeRadius * 2;
+      gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+      gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+      CreateGrid();
+   }
+
+   private void CreateGrid()
+   {
+      grid = new Node[gridSizeX, gridSizeY];
+      Vector3 bottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 -
+                           Vector3.forward * gridWorldSize.y / 2;
+      for (int y = 0; y < gridSizeY; y++)
+      {
+         for (int x = 0; x < gridSizeX; x++)
+         {
+            Vector3 worldPoint = bottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) +
+                                 Vector3.forward * (y * nodeDiameter + nodeRadius);
+            bool Wall = !Physics.CheckSphere(worldPoint, nodeRadius, WallMask);
+
+            grid[x, y] = new Node(Wall, worldPoint, x, y);
+         }
+      }
+   }
+
+   public Node NodeFromWorldPosition(Vector3 a_WorldPosition)
+   {
+      float xPoint = (a_WorldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
+      float yPoint = (a_WorldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y;
+
+      xPoint = Mathf.Clamp01(xPoint);
+      yPoint = Mathf.Clamp01(yPoint);
+
+      int x = Mathf.RoundToInt((gridSizeX - 1) * xPoint);
+      int y = Mathf.RoundToInt((gridSizeY - 1) * yPoint);
+
+      return grid[x, y];
+   }
+
+   public List<Node> GetNeighboringNodes(Node a_Node)
+   {
+      List<Node> NeighboringNodes = new List<Node>();
+
+      int xCheck;
+      int yCheck;
+      
+      //right Side
+      xCheck = a_Node.gridX + 1;
+      yCheck = a_Node.gridY;
+      if (xCheck >= 0 && xCheck < gridSizeX)
+      {
+         if (yCheck >= 0 && yCheck < gridSizeY)
+         {
+            NeighboringNodes.Add(grid[xCheck,yCheck]);
+         }
+      }
+      
+      //Left Side
+      xCheck = a_Node.gridX - 1;
+      yCheck = a_Node.gridY;
+      if (xCheck >= 0 && xCheck < gridSizeX)
+      {
+         if (yCheck >= 0 && yCheck < gridSizeY)
+         {
+            NeighboringNodes.Add(grid[xCheck,yCheck]);
+         }
+      }
+      
+      //Tom Side
+      xCheck = a_Node.gridX;
+      yCheck = a_Node.gridY + 1;
+      if (xCheck >= 0 && xCheck < gridSizeX)
+      {
+         if (yCheck >= 0 && yCheck < gridSizeY)
+         {
+            NeighboringNodes.Add(grid[xCheck,yCheck]);
+         }
+      }
+      
+      //Bottom Side
+      xCheck = a_Node.gridX;
+      yCheck = a_Node.gridY - 1;
+      if (xCheck >= 0 && xCheck < gridSizeX)
+      {
+         if (yCheck >= 0 && yCheck < gridSizeY)
+         {
+            NeighboringNodes.Add(grid[xCheck,yCheck]);
+         }
+      }
+
+      return NeighboringNodes;
+   }
+
+
+   private void OnDrawGizmos()
+   {
+      Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1 , gridWorldSize.y));
+
+      if (grid != null)
+      {
+         foreach (Node node in grid)
+         {
+            Gizmos.color = (node.IsWall) ? Color.white :  Color.red;
+            if(FinalPath != null)
+               if (FinalPath.Contains(node))
+                  Gizmos.color = Color.black;
+            
+            Gizmos.DrawCube(node.Position, Vector3.one * (nodeDiameter - Distance));
+         }
+      }
+   }
+}
